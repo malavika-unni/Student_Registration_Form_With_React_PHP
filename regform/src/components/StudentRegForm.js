69aa06b0c4b05a1f1a2a530294da7162b8f125de
Malavika Unni <malavikamcakuc@gmail.com>
2024-02-13 16:07:15 +0530

    Created structure of components in StudenRegForm.js and StudentView.js

---
+++ regform/src/components/StudentRegForm.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const StudentRegForm = ({ setUserData }) => {
+    
+    return;
+}
+
+export default StudentRegForm
--- regform/src/index.js
+++ regform/src/index.js
@@ -6,9 +6,11 @@ import StudentRegForm from './components/StudentRegForm';
 
 
 const App = () => {
+  // State to hold the submitted user data
   const [userData, setUserData] = useState(null);
   return (
     <div>
+      {/*Conditional rendering: Render StudentRegForm component if userData is null, else render StudentView component */}
       {!userData ? <StudentRegForm setUserData={setUserData} /> : <StudentView userData={userData} />}
     </div>
   );
